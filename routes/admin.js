76159b4e81d36fbe9d184aa07a505110751ac1dd
souptik4572 <souptiksarkar4572@gmail.com>
2021-05-13 20:55:55 +0530

    removed uneccessary code

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const User = require('../models/user');
-const Connection = require('../models/connection');
 const { isLoggedIn, isAdmin } = require('../middleware');
 const {
 	getAdminPanel,
